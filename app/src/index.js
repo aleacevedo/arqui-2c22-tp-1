8cdbd5adb31161cfec532c0297c8e255b5eee2ea
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2022-09-22 16:08:44 -0300

    Agregamos bbox, probamos locations y agregamos endpoint en el webserver

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// la idea era probar diferentes locations de ngnix y ver que esté todo funcionando bien. Esto es temporal.
+app.get('/bbox/x', (req, res) => {
+  return axios.get('http://bbox:9090')
+    .then(({ data }) => res.status(200).send(data))
+    .catch(err => res.status(500).send('Error no identificado'));
 });
 
 app.listen(3000, () => {
