bc96d43119c719128aca085ca87fd979af08c4b7
FrancoPrime <franco28xl@gmail.com>
2022-10-02 19:15:06 -0300

    feat: agregado statsd deamon a la aplicacion node

--- app/src/index.js
+++ app/src/index.js
@@ -1,26 +1,46 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const {DateTime} = require('luxon');
+var StatsD = require('node-statsd'),
+      client = new StatsD();
 
-app.get('/ping', (req, res) => {
-  console.log('ping');
+app.get('/ping', async (req, res) => {
+  const start = DateTime.now();
   res.send('pong');
+  const end = DateTime.now();
+  const responseTime = end - start;
+  client.timing('response_time', responseTime);
 });
 
 // la idea era probar diferentes locations de ngnix y ver que esté todo funcionando bien. Esto es temporal.
-app.get('/bbox/a', (req, res) => {
-  return axios.get('http:/localhost:5555/bbox/a/')
-    .then(({ data }) => res.status(200).send(data))
-    .catch(err => res.status(500).send('Error no identificado'));
+app.get('/bbox/a', async (req, res) => {
+  const start = DateTime.now();
+  try{
+    const response = await axios.get('http:/localhost:5555/bbox/a/');
+    res.status(200).send(response);
+  }catch(err){
+    res.status(500).send('Error no identificado')
+  }
+  const end = DateTime.now();
+  const responseTime = end - start;
+  client.timing('response_time', responseTime);
 });
 
 // la idea era probar diferentes locations de ngnix y ver que esté todo funcionando bien. Esto es temporal.
-app.get('/bbox/b', (req, res) => {
-  return axios.get('http:/localhost:5555/bbox/b/')
-    .then(({ data }) => res.status(200).send(data))
-    .catch(err => res.status(500).send('Error no identificado'));
+app.get('/bbox/b', async (req, res) => {
+  const start = DateTime.now();
+  try{
+    const response = await axios.get('http:/localhost:5555/bbox/b/')
+    res.status(200).send(response)
+  }catch(err){
+    res.status(500).send('Error no identificado')
+  }
+  const end = DateTime.now();
+  const responseTime = end - start;
+  client.timing('response_time', responseTime);
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Service started on port 3000!');
 });
